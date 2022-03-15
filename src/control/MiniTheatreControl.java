b5c7f455984c9b6c5920caceee925fd662d7a0dd
David Valderrama Basante <davidValderrama>
2022-03-14 22:51:33 -0500

    sala mini

--- src/control/MiniTheatreControl.java
+++ src/control/MiniTheatreControl.java
@@ -1,5 +1,403 @@
 package control;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+
 public class MiniTheatreControl {
+	
+	 @FXML
+	    private Button A1;
+
+	    @FXML
+	    private Button A2;
+
+	    @FXML
+	    private Button A3;
+
+	    @FXML
+	    private Button A4;
+
+	    @FXML
+	    private Button A5;
+
+	    @FXML
+	    private Button A6;
+
+	    @FXML
+	    private Button A7;
+
+	    @FXML
+	    private Button B8;
+
+	    @FXML
+	    private Button B9;
+
+	    @FXML
+	    private Button B10;
+
+	    @FXML
+	    private Button B11;
+
+	    @FXML
+	    private Button B12;
+
+	    @FXML
+	    private Button B13;
+
+	    @FXML
+	    private Button B14;
+
+	    @FXML
+	    private Button C15;
+
+	    @FXML
+	    private Button C16;
+
+	    @FXML
+	    private Button C17;
+
+	    @FXML
+	    private Button C18;
+
+	    @FXML
+	    private Button C19;
+
+	    @FXML
+	    private Button C20;
+
+	    @FXML
+	    private Button C21;
+
+	    @FXML
+	    private Button D22;
+
+	    @FXML
+	    private Button D23;
+
+	    @FXML
+	    private Button D24;
+
+	    @FXML
+	    private Button D25;
+
+	    @FXML
+	    private Button D26;
+
+	    @FXML
+	    private Button D27;
+
+	    @FXML
+	    private Button D28;
+	    
+	    @FXML
+		void changeColorA1(MouseEvent event) {
+
+			if (A1.getStyle() == "-fx-background-color: #000080; ") {
+				A1.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A1.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA2(MouseEvent event) {
+
+			if (A2.getStyle() == "-fx-background-color: #000080; ") {
+				A2.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A2.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA3(MouseEvent event) {
+
+			if (A3.getStyle() == "-fx-background-color: #000080; ") {
+				A3.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A3.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA4(MouseEvent event) {
+
+			if (A4.getStyle() == "-fx-background-color: #000080; ") {
+				A4.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A4.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA5(MouseEvent event) {
+
+			if (A5.getStyle() == "-fx-background-color: #000080; ") {
+				A5.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A5.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA6(MouseEvent event) {
+
+			if (A6.getStyle() == "-fx-background-color: #000080; ") {
+				A6.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A6.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorA7(MouseEvent event) {
+
+			if (A7.getStyle() == "-fx-background-color: #000080; ") {
+				A7.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				A7.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB8(MouseEvent event) {
+
+			if (B8.getStyle() == "-fx-background-color: #000080; ") {
+				B8.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B8.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB9(MouseEvent event) {
+
+			if (B9.getStyle() == "-fx-background-color: #000080; ") {
+				B9.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B9.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB10(MouseEvent event) {
+
+			if (B10.getStyle() == "-fx-background-color: #000080; ") {
+				B10.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B10.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB11(MouseEvent event) {
+
+			if (B11.getStyle() == "-fx-background-color: #000080; ") {
+				B11.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B11.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB12(MouseEvent event) {
+
+			if (B12.getStyle() == "-fx-background-color: #000080; ") {
+				B12.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B12.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB13(MouseEvent event) {
+
+			if (B13.getStyle() == "-fx-background-color: #000080; ") {
+				B13.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B13.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorB14(MouseEvent event) {
+
+			if (B14.getStyle() == "-fx-background-color: #000080; ") {
+				B14.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				B14.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC15(MouseEvent event) {
+
+			if (C15.getStyle() == "-fx-background-color: #000080; ") {
+				C15.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C15.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC16(MouseEvent event) {
+
+			if (C16.getStyle() == "-fx-background-color: #000080; ") {
+				C16.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C16.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC17(MouseEvent event) {
+
+			if (C17.getStyle() == "-fx-background-color: #000080; ") {
+				C17.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C17.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC18(MouseEvent event) {
+
+			if (C18.getStyle() == "-fx-background-color: #000080; ") {
+				C18.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C18.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC19(MouseEvent event) {
+
+			if (C19.getStyle() == "-fx-background-color: #000080; ") {
+				C19.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C19.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC20(MouseEvent event) {
+
+			if (C20.getStyle() == "-fx-background-color: #000080; ") {
+				C20.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C20.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorC21(MouseEvent event) {
+
+			if (C21.getStyle() == "-fx-background-color: #000080; ") {
+				C21.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				C21.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD22(MouseEvent event) {
+
+			if (D22.getStyle() == "-fx-background-color: #000080; ") {
+				D22.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D22.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD23(MouseEvent event) {
+
+			if (D23.getStyle() == "-fx-background-color: #000080; ") {
+				D23.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D23.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD24(MouseEvent event) {
+
+			if (D24.getStyle() == "-fx-background-color: #000080; ") {
+				D24.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D24.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD25(MouseEvent event) {
+
+			if (D25.getStyle() == "-fx-background-color: #000080; ") {
+				D25.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D25.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD26(MouseEvent event) {
+
+			if (D26.getStyle() == "-fx-background-color: #000080; ") {
+				D26.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D26.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD27(MouseEvent event) {
+
+			if (D27.getStyle() == "-fx-background-color: #000080; ") {
+				D27.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D27.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+		@FXML
+		void changeColorD28(MouseEvent event) {
+
+			if (D28.getStyle() == "-fx-background-color: #000080; ") {
+				D28.setStyle("-fx-background-color: #ff8000; ");
+			} else {
+				D28.setStyle("-fx-background-color: #000080; ");
+
+			}
+		}
+
+	    
 
 }
